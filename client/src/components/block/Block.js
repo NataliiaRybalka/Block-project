3f34d94af9c330245ab48d635cdf7b31089a6e30
NataliiaRybalka <natalirybalka21@gmail.com>
2022-01-27 20:39:04 +0200

    return powerbank

--- client/src/components/block/Block.js
+++ client/src/components/block/Block.js
@@ -2,13 +2,21 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import './Block.css';
-import { getPowerbanks, getUserPowerbank } from '../../redux/actions/block.actions';
+import { getPowerbanks, getUserPowerbank, returnMyPowerbank, changePowerbankInStock } from '../../redux/actions/block.actions';
 import { Powerbank } from "./Powerbank";
 
 export const Block = () => {
   const dispatch = useDispatch();
   const powerbanks = useSelector(state => state.blockReducer.powerbanks);
   const blocks = useSelector(state => state.blockReducer.blocks);
+  const myPowerBank = useSelector(state => state.blockReducer.myPowerBank);
+  const doReturn = useSelector(state => state.blockReducer.return);
+
+  for (let i = 0; i < blocks.length; i++) {
+    for (let j = 0; j < powerbanks.length; j++) {
+      blocks[i].powerbank = powerbanks[i];
+    }
+  }
 
   useEffect(() => {
     dispatch(getPowerbanks());
@@ -18,12 +26,28 @@ export const Block = () => {
     }
   }, [dispatch]);
 
+  const onSelectEmptyBlock = blockId => {
+    dispatch(returnMyPowerbank(myPowerBank));
+
+    powerbanks.map(powerbank => {
+      if (powerbank.id === myPowerBank) {
+        powerbank.in_stock = true;
+        powerbank.block_id = blockId;
+      }
+      return powerbank;
+    })
+
+    const powerbank = powerbanks.find(powerbank => powerbank.id === myPowerBank);
+    
+    dispatch(changePowerbankInStock(powerbank));
+  };
+
   return (
     <div id={'blocksContainer'}>
       {blocks?.map(block => (
-        <div className={'block'} key={block.id}>
-          <span className={'blockName'}>{block.id} Block</span>
-          {/* <Powerbank powerbank={powerbank} key={powerbank.id} /> */}
+        <div className={doReturn && !block.powerbank.in_stock ? 'block select' : 'block'} key={block.id}>
+          <span onClick={() => onSelectEmptyBlock(block.id)} className={'blockName'}>{block.id} Block</span>
+          <Powerbank powerbank={block.powerbank} key={block.powerbank.id} />
         </div>
       ))}
     </div>
--- client/src/components/block/Powerbank.js
+++ client/src/components/block/Powerbank.js
@@ -1,12 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import './Block.css';
-import { saveToMyPowerbank, changePowerbankInStock, returnMyPowerbank } from '../../redux/actions/block.actions';
+import { saveToMyPowerbank, changePowerbankInStock } from '../../redux/actions/block.actions';
 
 export const Powerbank = ({ powerbank }) => {
   const dispatch = useDispatch();
   const myPowerBank = useSelector(state => state.blockReducer.myPowerBank);
-  const doReturn = useSelector(state => state.blockReducer.return);
 
   const takePowerbank = (powerbank) => {
     if (!myPowerBank) {
@@ -19,21 +18,9 @@ export const Powerbank = ({ powerbank }) => {
     }
   };
 
-  const onSelectEmptyBlock = blockId => {
-    dispatch(returnMyPowerbank(myPowerBank));
-
-    powerbank.in_stock = true;
-    powerbank.block_id = blockId;
-
-    dispatch(changePowerbankInStock(powerbank));
-  };
-
   return (
-    // <div className={doReturn && !powerbank.in_stock ? 'block select' : 'block'}>
-    //   <span onClick={() => onSelectEmptyBlock(powerbank.block_id)} className={'blockName'}>{powerbank.block_id} Block</span>
-      <button onClick={() => takePowerbank(powerbank)} className={'powerbankBtn'}>
-        {powerbank.in_stock ? `in stock ${powerbank.id}` : 'empty'}
-      </button>
-    // </div>
+    <button onClick={() => takePowerbank(powerbank)} className={'powerbankBtn'}>
+      {powerbank.in_stock ? `in stock ${powerbank.id}` : 'empty'}
+    </button>
   );
 };
